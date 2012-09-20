e1931f8a2a3c09776cc90fb0a3944afe2260eb70
Jason Underhill <jay.underhill@gmail.com>
2012-09-20 11:31:49 +0100

    Written first test to load words file

--- lib/AnagramFinder.rb
+++ lib/AnagramFinder.rb
@@ -1,3 +1,5 @@
 class AnagramFinder
-
+	def LoadWords(filename)
+		File.readlines(filename).collect {|line| line.gsub(/[^a-z]/i, '')}
+	end
 end
--- spec/lib/AnagramFinder_spec.rb
+++ spec/lib/AnagramFinder_spec.rb
@@ -1,1 +1,11 @@
 require "AnagramFinder"
+
+describe "LoadWordsFile" do
+	before(:each) do
+		@anagramFinder = AnagramFinder.new
+	end
+	
+	it "should return an array with 45425 elements" do
+		@anagramFinder.LoadWords('dict45k.txt').length.should == 45425
+	end
+end
