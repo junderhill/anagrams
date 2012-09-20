2c95eb723dcf651c07935113157b4bc522575422
Jason Underhill <jay.underhill@gmail.com>
2012-09-20 11:35:10 +0100

    Tests for load words written and passing

--- spec/lib/AnagramFinder_spec.rb
+++ spec/lib/AnagramFinder_spec.rb
@@ -3,9 +3,17 @@ require "AnagramFinder"
 describe "LoadWordsFile" do
 	before(:each) do
 		@anagramFinder = AnagramFinder.new
+		@words = @anagramFinder.LoadWords('dict45k.txt')
 	end
 	
 	it "should return an array with 45425 elements" do
-		@anagramFinder.LoadWords('dict45k.txt').length.should == 45425
+		@words.length.should == 45425
+	end
+	
+	it "should return Aarhus as the first element in the array" do
+		@words.first.should == "Aarhus"
+	end
+	it "should return Zurich as the last element in the array" do
+		@words.last.should == "Zurich"
 	end
 end
